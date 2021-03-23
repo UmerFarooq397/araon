62486eede8776810a0582902f4f4e83fdc6d1545
HP <umerfarooq@hotmail.com>
2021-03-23 18:59:26 +0500

    first commit

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { loadStripe } from '@stripe/stripe-js';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+  title = 'angular-stripe';
+  priceId = 'price_1IXWjmKIi9cViIYizqzYff35';
+  product = {
+    title: 'ElonCoin',
+    subTitle: 'Buy 50 Elon coin',
+    description: 'you can get 50 Eloncoins for 50$',
+    price: 50.00
+  };
+  quantity = 1;
+  stripePromise = loadStripe(environment.stripe_key);
+
+  async checkout() {
+    // Call your backend to create the Checkout session.
+
+    // When the customer clicks on the button, redirect them to Checkout.
+    const stripe = await this.stripePromise;
+    const { error } = await stripe.redirectToCheckout({
+      mode: 'payment',
+      lineItems: [{ price: this.priceId, quantity: this.quantity }],
+      successUrl: `${window.location.href}/success`,
+      cancelUrl: `${window.location.href}/failure`,
+    });
+    // If `redirectToCheckout` fails due to a browser or network
+    // error, display the localized error message to your customer
+    // using `error.message`.
+    if (error) {
+      console.log(error);
+    }
+
+  }
+}
